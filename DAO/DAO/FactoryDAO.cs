1bf6051933c04b71cff80586051b2cb456b6b409
Felipe888 <38811678+Felipe888@users.noreply.github.com>
2018-05-05 20:47:47 -0500

    DAOPrueb
    Prueba

---
+++ DAO/DAO/FactoryDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class FactoryDAO : InterfaseDAO
+    {
+      private readonly Dictionary<int, Pregunta> _preguntaRepository = new Dictionary<int,Pregunta>();
+
+      
+      }
+       
+      
+    }
+}
---
+++ DAO/DAO/InterfaseDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public interface InterfaseDAO
+    {
+        public List<Pregunta> getPregunta();
+        public Pregunta getPreguntaTipo(string tipo);
+        public Pregunta getPreguntaValor(double valor);
+        public void calificacionPregunta(Pregunta pregunta);
+        Pregunta GetById(int id);
+        bool Update(Pregunta pregunta);
+    }
+}
