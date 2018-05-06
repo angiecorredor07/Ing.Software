267ba976564138efab8f63e2a0e602188a923334
Felipe888 <38811678+Felipe888@users.noreply.github.com>
2018-05-05 21:06:09 -0500

    Proto

---
+++ Prototype/Programa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    class Programa
+    {
+        static void Main(string[] args) {
+            Prototype prototype = new Prototype();
+            prototype.Class = "pregunta";
+            prototype.valor = 0.2;
+
+            Prototype Pregunta = prototype.Clone() as Prototype;
+            Pregunta.Class = "Verdadero y Falso";
+            Pregunta.valor = 0.2;
+
+            Prototype Pregunta1 = prototype.Clone() as Prototype;
+            Pregunta1.Class = "Unir Columnas";
+            Pregunta1.valor = 0.3; 
+        }
+    }
+}
---
+++ Prototype/Prototype.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    class Prototype
+    {
+        public string Class { get; set; }
+        public double valor { get; set; }
+        public Prototype Clone() {
+            return MemberwiseClone() as Prototype;
+        }
+    }
+}
